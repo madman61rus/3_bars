d72475ac430cf608b4891907af6d517376a4a979
madman61rus <rhjnhjtn3yjhe>
2016-12-26 13:40:12 +0300

    + Поменял название переменной в main c data на json_data

--- bars.py
+++ bars.py
@@ -31,9 +31,9 @@ def get_closest_bar(data, longitude, latitude):
 
 if __name__ == '__main__':
     file_path = input('Введите путь к файлу ')
-    data = load_data(file_path)
+    json_data = load_data(file_path)
     latitude = float(input('Введите latitude '))
     longitude = float(input('Введите longitude '))
-    print('Самый маленький бар - ' + get_smallest_bar(data))
-    print('Самый большой бар - ' + get_biggest_bar(data))
-    print('Самый ближайший бар - ' + get_closest_bar(data,longitude,latitude))
+    print('Самый маленький бар - ' + get_smallest_bar(json_data))
+    print('Самый большой бар - ' + get_biggest_bar(json_data))
+    print('Самый ближайший бар - ' + get_closest_bar(json_data,longitude,latitude))
